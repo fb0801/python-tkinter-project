82b8ee1b74c53353d832705a0b5aed08838ddb5e
fb0801 <74499472+fb0801@users.noreply.github.com>
2021-09-05 17:55:51 +0100

    Add files via upload

--- tkinter_project.py
+++ tkinter_project.py
@@ -1,10 +1,23 @@
 from tkinter import *
 
 root =Tk()
+root.title('Calculator')
 
-#creating lbl widget
-myLabel = Label(root, text="Hello word!")
-myLabel.pack()
+
+e = Entry(root, width=35, borderwidth=5)
+e.grid(row=0,column=0, columnspan=3, padx=10, pady=10)
+
+e.insert(0, "Enter your number")
+
+def myClick():
+    hello = "hello" + e.get()
+    
+
+    #creating lbl widget
+    myLabel = Label(root, text="Hello word!")
+    myLabel.pack()
+
+mybtn = Button(root, text="enter your number", command =myClick)
 
 
 
