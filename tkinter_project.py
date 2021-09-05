4ade0d86d16f485c91495f03b3e4bf6d2ce4f78f
fb0801 <74499472+fb0801@users.noreply.github.com>
2021-09-05 18:18:25 +0100

    Add files via upload

--- tkinter_project.py
+++ tkinter_project.py
@@ -7,12 +7,25 @@ root.title('Calculator')
 e = Entry(root, width=35, borderwidth=5)
 e.grid(row=0,column=0, columnspan=3, padx=10, pady=10)
 
+def button_add():
+    return
 
 
-btn_1 = Button(root, text="1", padx=40,pady=20, command=button_add)
-btn_2 = Button(root, text="1", padx=40,pady=20, command=button_subtract)
-btn_3 = Button(root, text="1", padx=40,pady=20, command=button_divide)
-btn_4 = Button(root, text="1", padx=40,pady=20, command=button_multiply)
+def button_subtract():
+    return
+
+def button_divide():
+    return
+
+def button_multiply():
+    return
+
+
+
+btn_1 = Button(root, text="+", padx=40,pady=20, command=button_add)
+btn_2 = Button(root, text="-", padx=40,pady=20, command=button_subtract)
+btn_3 = Button(root, text="/", padx=40,pady=20, command=button_divide)
+btn_4 = Button(root, text="X", padx=40,pady=20, command=button_multiply)
 
 mybtn = Button(root, text="enter your number", command =myClick)
 
