1fcbcc4b5e47419eef2f8a8693d36e8a093a2d8c
fb0801 <74499472+fb0801@users.noreply.github.com>
2021-09-05 18:13:13 +0100

    Add files via upload

--- tkinter_project.py
+++ tkinter_project.py
@@ -9,7 +9,10 @@ e.grid(row=0,column=0, columnspan=3, padx=10, pady=10)
 
 
 
-
+btn_1 = Button(root, text="1", padx=40,pady=20, command=button_add)
+btn_2 = Button(root, text="1", padx=40,pady=20, command=button_subtract)
+btn_3 = Button(root, text="1", padx=40,pady=20, command=button_divide)
+btn_4 = Button(root, text="1", padx=40,pady=20, command=button_multiply)
 
 mybtn = Button(root, text="enter your number", command =myClick)
 
