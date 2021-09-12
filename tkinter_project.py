c4d3bda5820158254dd4726dcaa815b81e630cad
fb0801 <74499472+fb0801@users.noreply.github.com>
2021-09-12 19:19:41 +0100

    Add files via upload

--- tkinter_project.py
+++ tkinter_project.py
@@ -9,7 +9,8 @@ e.grid(row=0,column=0, columnspan=3, padx=10, pady=10)
 
 def button_click(number):
     #e.delete(0,END) #delete what alredy there
-    e.insert(0,number) #add it to the box
+    current = e.get()
+    e.insert(0, str(current) + str(number) #add it to the box
 
 def button_add():
     return
