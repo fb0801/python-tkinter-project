ee5aa2048cd04bffafb8174ad852e4ad65681318
fb0801 <74499472+fb0801@users.noreply.github.com>
2021-09-15 15:49:07 +0100

    Add files via upload

--- images.py
+++ images.py
@@ -1,8 +1,10 @@
 from tkinter import *
 
 root = Tk()
-root.title(learn to code)
+root.title('learn to code')
+root.iconbitmap()
 
-
+button_quit =Button(root, text="Exit program", command=root.quit)
+button_quit.pack()
 
 root.mainloop()
